143ba62750cc450a9e38c4b208765f689f3aff16
SomeoneSomebody <hverstrat@hotmail.com>
2017-02-18 10:53:25 -0500

    cameraTurn

--- robot.py
+++ robot.py
@@ -45,7 +45,9 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        if self.stick.getRawButton(9):
+        if self.stick.getRawButton(5):
+            self.robot_drive.arcadeDrive(0.75*self.stick.getY(), self.table.getNumber("cameraX", 0))
+        elif self.stick.getRawButton(9):
             self.robot_drive.arcadeDrive(1*self.stick.getY(),-1*self.stick.getX())
         else:
             self.robot_drive.arcadeDrive(0.75*self.stick.getY(),-0.75*self.stick.getX())
